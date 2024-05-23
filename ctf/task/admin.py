dda6753c504b1cd82ba8a66bb36a172a7af55a8f
incelcure <sergeekoilia99@gmail.com>
2024-05-23 23:11:03 +0300

    added admin panel

--- ctf/task/admin.py
+++ ctf/task/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Task
 
-# Register your models here.
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'file_url', 'answer')
+    search_fields = ('title', 'description')
+    list_filter = ('title',)
+
+
+admin.site.register(Task, TaskAdmin)
