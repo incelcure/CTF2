79956b072e3a9b7e3792562b75ce5e50eef0ad30
Head Gardener <trashbin2019np@gmail.com>
2024-05-28 20:02:50 +0300

    fix: runs migrations from the container

--- nix/image.nix
+++ nix/image.nix
@@ -1,22 +1,25 @@
 {pkgs, self', ...}:
 
+let
+  python = "${self'.packages.default.dependencyEnv}/bin/python";
+  entrypoint = pkgs.writeShellScriptBin "entrypoint.sh" ''
+    ${python} ${../ctf}/manage.py makemigrations
+    ${python} ${../ctf}/manage.py migrate
+    ${python} ${../ctf}/manage.py runserver 0.0.0.0:8000
+  '';
+in
+
 pkgs.dockerTools.buildImage {
   name = "ctf";
 
-  # copyToRoot = pkgs.buildEnv {
-  #   name = "image-root";
-  #   paths = with pkgs; [ ];
-  #   pathsToLink = [ "/bin" ];
-  # };
+  copyToRoot = pkgs.buildEnv {
+    name = "image-root";
+    paths = with pkgs; [ entrypoint ];
+    pathsToLink = [ "/bin" ];
+  };
 
   config = {
-    Cmd = [
-      "${self'.packages.default.dependencyEnv}/bin/python"
-      "${../ctf}/manage.py"
-      "runserver"
-      "0.0.0.0:8000"
-    ];
+    Cmd = [ "/bin/entrypoint.sh" ];
     ExposedPorts = { "8000/tcp" = { }; };
-    # ExtraCommands = '' '';
   };
 }
