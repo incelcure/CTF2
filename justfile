b1371bc335d0d88196c7151e6ef5f688197b849a
Head Gardener <trashbin2019np@gmail.com>
2024-06-01 19:36:56 +0300

    feat(infra): adds load-tasks job

--- justfile
+++ justfile
@@ -1,9 +1,9 @@
-push-image:
-  docker tag $(docker load < $(nix build --print-out-paths .#image) | cut -d' ' -f 3) localhost:5001/ctf:latest
+push-image target="server":
+  docker tag $(docker load < $(nix build --print-out-paths .#{{target}}) | cut -d' ' -f 3) localhost:5001/ctf:latest
   docker push localhost:5001/ctf:latest
 
-run-container:
-  docker run -it $(docker load < $(nix build --print-out-paths .#image) | cut -d' ' -f 3)
+run-container target="server":
+  docker run -it $(docker load < $(nix build --print-out-paths .#{{target}}) | cut -d' ' -f 3)
 
 encrypt:
   for f in $(ls infra/*-secret.yaml); do gpg --encrypt --recipient trashbin2019np@gmail.com --recipient blueberry --recipient cherry --batch --yes $f; done
--- nix/image.nix
+++ nix/image.nix
@@ -1,25 +1,30 @@
-{pkgs, self', ...}:
+{ pkgs, self', ... }:
 
 let
-  python = "${self'.packages.default.dependencyEnv}/bin/python";
+  python = self'.packages.default.dependencyEnv;
   entrypoint = pkgs.writeShellScriptBin "entrypoint.sh" ''
-    ${python} ${../ctf}/manage.py makemigrations
-    ${python} ${../ctf}/manage.py migrate
-    ${python} ${../ctf}/manage.py runserver 0.0.0.0:8000
+    python /manage.py makemigrations
+    python /manage.py migrate
+    python /manage.py runserver 0.0.0.0:8000
   '';
 in
 
-pkgs.dockerTools.buildImage {
-  name = "ctf";
+{
+  server = pkgs.dockerTools.buildImage {
+    name = "ctf";
 
-  copyToRoot = pkgs.buildEnv {
-    name = "image-root";
-    paths = with pkgs; [ entrypoint ];
-    pathsToLink = [ "/bin" ];
-  };
+    copyToRoot = [
+      ../ctf
+      (pkgs.buildEnv {
+        name = "image-root";
+        paths = with pkgs; [ python entrypoint ];
+        pathsToLink = [ "/bin" ];
+      })
+    ];
 
-  config = {
-    Cmd = [ "/bin/entrypoint.sh" ];
-    ExposedPorts = { "8000/tcp" = { }; };
+    config = {
+      Cmd = [ "/bin/entrypoint.sh" ];
+      ExposedPorts = { "8000/tcp" = { }; };
+    };
   };
 }
